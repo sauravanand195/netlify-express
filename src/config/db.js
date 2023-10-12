a67af752597d0006c7a0b869d646c1223a888ba9
saurav anand <saurav.anand1@justdial.com>
2023-10-12 13:00:59 +0530

    db config updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-require('dotenv').config();
+require('dotenv').config()
 const dbUrl = process.env.DATABASE_URL
 
 module.exports.mongoConnection = mongoose.connect(dbUrl, {
