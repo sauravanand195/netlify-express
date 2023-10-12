77f721c70fe0d7694a7c7672fa497f97abd72d3e
saurav anand <saurav.anand1@justdial.com>
2023-10-12 16:11:56 +0530

    typeof

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
 let dbUrl = process.env.DATABASE_URL
-dbUrl = dbUrl.toString()
 
-module.exports.mongoConnection = mongoose.connect(`${dbUrl}`, {
-    //must add in order to not get any error masseges:
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, (err) => {
-    if (!err) {
-        console.log('Db connection is successfull');
-    } else {
-        console.log(`Error connecting Db: ${err}`);
-    }
-})
+const mongoConnection = () => {
+    dbUrl = dbUrl.toString()
+    console.log('dbUrl :>> ', dbUrl, typeof (dbUrl));
+    mongoose.connect(`${dbUrl}`, {
+        //must add in order to not get any error masseges:
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, (err) => {
+        if (!err) {
+            console.log('Db connection is successfull');
+        } else {
+            console.log(`Error connecting Db: ${err}`);
+        }
+    })
+}
+
+module.exports = mongoConnection
