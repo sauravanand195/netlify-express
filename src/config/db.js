d1630d652c58b85fd62f1a1ae6ba6e2d69d045d3
saurav anand <saurav.anand1@justdial.com>
2023-10-12 15:47:19 +0530

    db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const dbUrl = process?.env?.DATABASE_URL
 
-module.exports.mongoConnection = mongoose.connect(process.env.DATABASE_URL, {
+module.exports.mongoConnection = mongoose.connect(dbUrl, {
     //must add in order to not get any error masseges:
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-    useNewUrlParser: true
 }, (err) => {
     if (!err) {
         console.log('Db connection is successfull');
