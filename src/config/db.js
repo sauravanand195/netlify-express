c5a55ce2e8483f8aebd55b39a28b249cb6dbb716
saurav anand <saurav.anand1@justdial.com>
2023-10-12 12:56:09 +0530

    db config updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config();
 const dbUrl = process.env.DATABASE_URL
 
 module.exports.mongoConnection = mongoose.connect(dbUrl, {
-    useNewUrlParser : "true",
-    useUnifiedTopology : "true"
-}, (err)=>{
-    if(!err){
+    useNewUrlParser: "true",
+    useUnifiedTopology: "true"
+}, (err) => {
+    if (!err) {
         console.log('Db connection is successfull');
     } else {
         console.log(err);
